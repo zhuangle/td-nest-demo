101f2197d0cd787b1f8e69944f32778225aa926b
zhuang <acdsse@163.com>
2024-05-21 09:24:14 +0800

    跨域配置恢复到以前

--- src/main.ts
+++ src/main.ts
@@ -10,10 +10,17 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options)
   // app.use(cors())
   const corsOptions = {
-    origin: true, // 允许所有来源
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // 允许的 HTTP 请求方法
-    credentials: true, // 启用凭证
+    origin: 'http://localhost:3002', // 设置允许跨域的来源
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true, // 允许发送身份验证凭据（例如 cookies）
   };
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3002'); // 设置允许跨域的来源
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
   // 应用 CORS 中间件
   app.use(cors(corsOptions));
 
@@ -23,3 +30,4 @@ async function bootstrap() {
   await app.listen(3000);
 }
 bootstrap();
+
